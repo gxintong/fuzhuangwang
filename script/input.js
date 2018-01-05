353f6e8ab066113e11c258b1096a87624f70b0a1
guanxintong <626411757@qq.com>
2018-01-05 16:02:57 +0800

    first commit

---
+++ script/input.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+$("#inputSearch").focus(function(){
+$("this").addClass("focus");
+if($(this).val()==this.defaultValue){
+	$(this).val("");
+}
+}).blur(function(){
+	$("this").removeClass("focus")
+	if($(this).val()==""){
+		$(this).val(this.defaultValue)
+	}
+
+}).keyup(function(e){
+	if(e.which==13){
+		alert("回车提交表单!");
+	}
+})
+
+
+
+
+});
+
